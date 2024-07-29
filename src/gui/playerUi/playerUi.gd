3f9ce515df968df71c7f18e1c1d6706b7b7a1e25
raincuhh <146904648+raincuhh@users.noreply.github.com>
2024-07-29 21:34:01 +0200

    Revert "after commit"
    This reverts commit 45fb830c7f72bcc2e7e862f8509b4d9ab3963419.

--- src/gui/playerUi/playerUi.gd
+++ src/gui/playerUi/playerUi.gd
@@ -1,19 +1,40 @@
 extends CanvasLayer
 
 @onready var player: CharacterBody2D = NodeUtility.get_player()
-@onready var staminaBar: ProgressBar = $Control/MarginContainer/Stamina/MarginContainer/Control/StaminaBar
+
+@onready var staminaBar: TextureProgressBar = $Control/MarginContainer/Stamina/MarginContainer/Control/StaminaBar
+@onready var stopwatchLabel: Label = $Control/MarginContainer/Stopwatch/MarginContainer/Control/StopwatchLabel
 
 #vars
 var maxStamina: float
 var currentStamina: float
 
+var time: float = 0.0
+var milliseconds: float = 0.0
+var seconds: float = 0.0
+var minutes: float = 0.0
+
 func _ready() -> void:
 	init_stamina_bar()
-	#signals
+	init_stopwatch_timer()
+	
 	player.climbStaminaChanged.connect(on_stamina_changed)
 
 func _process(delta: float) -> void:
-	pass
+	update(delta)
+
+func update(delta: float) -> void:
+	#time
+	time += delta
+	milliseconds = fmod(time, 1) * 1000
+	seconds = fmod(time, 60)
+	minutes = fmod(time, 3600) / 60
+	stopwatchLabel.text = "%03d: %02d. %02d" %[milliseconds, seconds, minutes]
+	
+	#
+
+func init_stopwatch_timer() -> void:
+	stopwatchLabel.text = "000: 00. 00"
 
 func init_stamina_bar() -> void:
 	maxStamina = player.climbMaxStamina
@@ -22,10 +43,8 @@ func init_stamina_bar() -> void:
 	staminaBar.min_value = 0.0
 	staminaBar.max_value = maxStamina
 	staminaBar.value = currentStamina
+	staminaBar.tint_progress = Color("#1c9393") #1c9393
 
 func on_stamina_changed(amount: float) -> void:
 	currentStamina = amount
 	staminaBar.value = currentStamina
-
-
-
---
+++ src/scenes/objects/dashRefillOrb/dashRefillOrb.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var player: CharacterBody2D = NodeUtility.get_player()
+
+var rotSpeed: float = 0.05
+
+func _on_area_2d_body_entered(body) -> void:
+	if !body == player:
+		return
+	
+	player.refill_dashes()
+	queue_free()
+
+func _process(delta: float) -> void:
+	rotation += rotSpeed
